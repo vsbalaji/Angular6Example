155bedf03cb8ea6a1c4eb904d0b26eef481e52ee
Balaji Sankaran <vsbalaji.mca@gmail.com>
2018-06-11 09:12:53 +0530

    folder structure created

--- AngularExample/src/app/app.module.ts
+++ AngularExample/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './views/template-form/template-form.component';
+import { ReactiveFormComponent } from './views/reactive-form/reactive-form.component';
+import { AttributeDirectiveDirective } from './directives/attribute-directive.directive';
+import { StructuralDirectiveDirective } from './directives/structural-directive.directive';
+import { LoginComponent } from './views/login/login.component';
+import { LayoutComponent } from './views/layout/layout.component';
+import { CustomPipePipe } from './pipes/custom-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent,
+    AttributeDirectiveDirective,
+    StructuralDirectiveDirective,
+    LoginComponent,
+    LayoutComponent,
+    CustomPipePipe
   ],
   imports: [
     BrowserModule,
---
+++ AngularExample/src/app/directives/attribute-directive.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appAttributeDirective]'
+})
+export class AttributeDirectiveDirective {
+
+  constructor() { }
+
+}
